d3f9b35311023bcff9272104d372a9ec4d48d8aa
Daniel Ben Harosh <>
2022-05-04 21:05:02 +0300

    copy pipeline out of jenkins web ui into a pipeline.groovy file to edit in vscode

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -1,30 +1,28 @@
 pipeline {
     agent any
-    triggers { pollSCM('* * * * *') }
+
     stages {
         stage('Checkout') {
             steps {
-                git url: 'https://github.com/g0t4/jgsu-spring-petclinic.git', branch: 'main'
-            }            
-        }
+                // Get some code from a GitHub repository
+                git branch: 'main', url: 'https://github.com/Egozi888/jgsu-spring-petclinic.git'
+            }
+        }                    
         stage('Build') {
             steps {
-                sh './mvnw clean package'
-                //sh 'false' // true
+                // Get some code from a GitHub repository
+                // git branch: 'main', url: 'https://github.com/Egozi888/jgsu-spring-petclinic.git'
+
+                // Run Maven on a Unix agent.
+                sh "./mvnw package"
             }
-        
+
             post {
+                // If Maven was able to run the tests, even if some of the test
+                // failed, record the test results and archive the jar file.
                 always {
-                    junit '**/target/surefire-reports/TEST-*.xml'
+                    junit 'target/surefire-reports/*.xml'
                     archiveArtifacts 'target/*.jar'
-                }
-                changed {
-                    emailext subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}",
-                        body: "Please go to ${BUILD_URL} and verify the build", 
-                        attachLog: true, 
-                        compressLog: true, 
-                        to: "test@jenkins",
-                        recipientProviders: [upstreamDevelopers(), requestor()]
                 }
             }
         }
